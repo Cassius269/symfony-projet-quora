61fd120dc0666cb4d35dfa9aa9a94d20dca5499d
cassius <fahamygaston@gmail.com>
2024-04-17 13:47:52 +0200

    mise en place de la stylisation de l'aside avec les interactions des boutons

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,22 @@
 console.log('hello wonder');
+
+//Sélection de la nav de l'aiside
+let navAside = document.querySelector("nav");
+
+console.log(navAside);
+
+/*
+selectionner la nav qui est parent
+Event
+Si est un bouton de classe "asideNavButton" alors ajouter la classe avec toggle */
+
+navAside.addEventListener('click', (e)=>{
+    console.log(e.target);
+console.log(e)
+    if(e.target.tagName==="BUTTON" && e.target.classList.contains("asideNavButton")){
+        console.log("Je suis un bouton asideNavButton");
+
+        e.target.classList.toggle("navButtonActive");
+    }
+
+})
