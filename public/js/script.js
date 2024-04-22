0f721f26fd40bcf799dadbf9bf681310a7dc185b
cassius <fahamygaston@gmail.com>
2024-04-22 14:38:32 +0200

    debogage du script js pour l'aside

--- public/js/script.js
+++ public/js/script.js
@@ -13,10 +13,22 @@ Si est un bouton de classe "asideNavButton" alors ajouter la classe avec toggle 
 navAside.addEventListener('click', (e)=>{
     console.log(e.target);
 console.log(e)
-    if(e.target.tagName==="BUTTON" && e.target.classList.contains("asideNavButton")){
-        console.log("Je suis un bouton asideNavButton");
-
+    if((e.target.tagName==="BUTTON" &&  e.target.classList.contains("asideNavButton")) ){
+            
+        console.log("Je suis un button asideNavButton");
         e.target.classList.toggle("navButtonActive");
+        e.target.classList.toggle("leftbarre");
+
+    }else if (e.target.tagName==="IMG" &&  e.target.parentNode.parentNode.classList.contains("asideNavButton")){
+        
+        console.log("Je suis une balise image de la liste d'option");
+        e.target.parentNode.parentNode.classList.toggle("navButtonActive");
+        e.target.parentNode.parentNode.classList.toggle("leftbarre");
+
+    }else if(e.target.tagName==='A' && e.target.parentNode.classList.contains("asideNavButton")){
+        console.log('Je suis une balise <a>');
+        e.target.parentNode.classList.toggle("navButtonActive");
+        e.target.parentNode.classList.toggle("leftbarre");
     }
 
 })
