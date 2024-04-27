cf0b9c6791080609f989fb74e82e08d35687994a
cassius <fahamygaston@gmail.com>
2024-04-27 07:55:04 +0200

    optimisation des routes dans le questionController

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -10,53 +10,56 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+#[Route(path: '/question', name: 'question_')]
 class QuestionController extends AbstractController
 {
     #[Route(
-        path: '/question/{param}',
-        name: 'asqQuestion'
+        path: '/ask',
+        name: 'askForm'
     )]
-    public function index(Request $request, string|int $param = 'ask'): Response
+    public function index(Request $request): Response
     {
 
-        if ($param === 'ask') {
-            // Créer une nouvelle instance de l'objet question 
-            $question = new Question();
+        // Créer une nouvelle instance de l'objet question 
+        $question = new Question();
 
-            // Créer le formulaire
-            $form = $this->createForm(QuestionType::class, $question);
+        // Créer le formulaire
+        $form = $this->createForm(QuestionType::class, $question);
 
-            // Reccuillir la requete
-            $form->handleRequest($request);
+        // Reccuillir la requete
+        $form->handleRequest($request);
 
-            // Traiter le formulaire
-            if ($form->isSubmitted() && $form->isValid()) {
-                //dd($form->getData());
-                //array_push(HomeController::$questions, $form->getData());
-            }
-            // Rendre la vue du formulaire
-            return $this->render('question/index.html.twig', [
-                'askForm' => $form->createView()
-            ]);
-        } elseif ($param != "ask" && is_numeric($param) == false) {
-            throw $this->createNotFoundException('Le paramètre n\'est pas un id disponible ni \'ask\' par défaut');
+        // Traiter le formulaire
+        if ($form->isSubmitted() && $form->isValid()) {
+            //dd($form->getData());
+            //array_push(HomeController::$questions, $form->getData());
         }
+        // Rendre la vue du formulaire
+        return $this->render('question/index.html.twig', [
+            'askForm' => $form->createView()
+        ]);
+    }
 
-        if (is_numeric($param)) {
-            // stocker la variable question 
-            $questions = HomeController::$questions;
+    #[Route(
+        path: '/{id}',
+        name: 'detail'
+    )]
+    public function showQuestionDetail(int $id)
+    {
+        // stocker la variable question 
+        $questions = HomeController::$questions;
 
-            // verifier si le paramètre numérique entré appartient au tableau des questions
-            $questionsById = array_column($questions, 'id');
+        // verifier si le paramètre numérique entré appartient au tableau des questions
+        $questionsById = array_column($questions, 'id');
 
-            $result = in_array($param, $questionsById, $strict = true);
+        $result = in_array($id, $questionsById, $strict = true);
 
-            // Si ok retourner la vue de la question en détail
-            // sinon rediriger l'utilisateur avec un code d'erreur 404
-            if ($result == true || isset($questionsById["$param"]) != true) {
-                throw $this->createNotFoundException('L\'ID entré est invalide et indispobible dans le tableau des questions');
-            }
-            return $this->render('question/detailledQuestion.html.twig', ["question" => $questions[$param]]);
+        // Si ok retourner la vue de la question en détail
+        // sinon rediriger l'utilisateur avec un code d'erreur 404
+        //dd($questions[$id]]);
+        if ($result != true) {
+            throw $this->createNotFoundException('L\'ID entré est invalide et indispobible dans le tableau des questions');
         }
+        return $this->render('question/detailledQuestion.html.twig', ["question" => $questions[$id - 1]]);
     }
 }
